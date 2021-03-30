ddd2ba3d2ba695be6ebb4c20eda4ecc38aad8763
yjimenez <yjimenez@sonnysdirect.com>
2021-03-30 17:22:27 -0400

    Adding SendGrid Mailer support.

---
+++ src/Communication/Mailer/SendGrid/Exception/MailerException.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @author Yenier Jimenez <yjmorales86@gmail.com>
+ */
+
+namespace App\Mailer\SendGrid\Exception;
+
+use Exception;
+
+/**
+ * Exception to be thrown once a Mailer fails to send emails.
+ *
+ * @package App\Mailer\SendGrid\Exception
+ */
+class MailerException extends Exception
+{
+
+}
