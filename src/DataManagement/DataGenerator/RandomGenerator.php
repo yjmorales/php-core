41edc4e5a5f56536c92075fc343ec20189e66a90
yjimenez <yjimenez@sonnysdirect.com>
2022-11-14 12:37:54 -0500

    Adding a Random Generator used to populate resources. e:g. databases.

---
+++ src/DataManagement/DataGenerator/RandomGenerator.php
@@ -0,0 +1,294 @@
+<?php
+/**
+ * @author Yenier Jimenez <yjmorales86@gmail.com>
+ */
+
+namespace Common\DataManagement\DataGenerator;
+
+use DateTime;
+use Exception;
+
+/**
+ * Class responsible to generate utils random data used to populate a database or other resources that needs initial
+ * data to operate.
+ */
+class RandomGenerator
+{
+    /**
+     * Generates an username
+     *
+     * @return string
+     */
+    public static function generateUsername(): string
+    {
+        // Returns a specific char between a-z
+        $letter   = chr(rand(97, 122));
+        $lastName = self::generateLastName();
+
+        return strtolower($letter . $lastName);
+    }
+
+    /**
+     * Generates a email address.
+     *
+     * @return string
+     */
+    public static function generateEmail(): string
+    {
+        $username    = self::generateUsername();
+        $emailServer = self::generateEmailServerName();
+        $domain      = self::generateDomain();
+
+        return "$username@$emailServer.$domain";
+    }
+
+    /**
+     * Generates a ser of emails address.
+     *
+     * @param int $limit Numbers of emails to generate.
+     *
+     * @return string[]
+     */
+    public static function generateEmails(int $limit = 20): array
+    {
+        $result  = [];
+        $names   = SeedValues::getNames();
+        $domains = SeedValues::getEmailDomains();
+        for ($i = 0; $i < $limit; $i++) {
+            $name      = $names[array_rand($names)];
+            $domain    = $domains[array_rand($domains)];
+            $result [] = strtolower($name) . rand(1, 2019) . '@' . $domain;
+        }
+
+        return $result;
+    }
+
+    /**
+     * Generates a set of phones numbers.
+     *
+     * @param int $limit Numbers of phones to generate.
+     *
+     * @return string[]
+     */
+    public static function generatePhonesNumbers(int $limit = 20): array
+    {
+        $result      = [];
+        $phonesCodes = SeedValues::getPhonesCodes();
+        for ($i = 0; $i < $limit; $i++) {
+            $code      = $phonesCodes[array_rand($phonesCodes)];
+            $result [] = '(' . $code . ') ' . rand(0, 9) . rand(0, 9) . rand(0, 9) . ' ' . rand(0, 9) . rand(0,
+                    9) . rand(0, 9);
+        }
+
+        return $result;
+    }
+
+    /**
+     * Generate a set of names.
+     *
+     * @param int $limit
+     *
+     * @return string[]
+     */
+    public static function generateFullNames(int $limit = 20): array
+    {
+        $names     = SeedValues::getNames();
+        $lastNames = SeedValues::getLastnames();
+        $result    = [];
+        for ($i = 0; $i < $limit; $i++) {
+            $name      = $names[array_rand($names)];
+            $lastName  = $lastNames[array_rand($lastNames)];
+            $result [] = "$name $lastName";
+        }
+
+        return $result;
+    }
+
+    /**
+     * Generate a set of address.
+     *
+     * @param int $limit
+     *
+     * @return string[]
+     */
+    public static function generateAddress(int $limit = 20): array
+    {
+        $result  = [];
+        $typesSt = SeedValues::getTypeSt();
+        $sts     = SeedValues::getStNames();
+        $cities  = SeedValues::getCities();
+        $states  = SeedValues::getStates();
+
+        for ($i = 0; $i < $limit; $i++) {
+            $typeSt    = $typesSt[array_rand($typesSt)];
+            $st        = $sts[array_rand($sts)];
+            $city      = $cities[array_rand($cities)];
+            $state     = $states[array_rand($states)];
+            $result [] = rand(1, 9999) . " $st $typeSt, $city, $state, " . rand(10000, 99999);
+        }
+
+        return $result;
+    }
+
+    /**
+     * Generates a gender
+     *
+     * @return string
+     */
+    public static function generateGender(): string
+    {
+        $result = 'F';
+        $gender = rand(1, 2);
+        if (1 === $gender) {
+            $result = 'M';
+        }
+
+        return $result;
+    }
+
+    /**
+     * Generates a valid datetime
+     *
+     * @return DateTime
+     *
+     * @throws Exception
+     */
+    public static function generateDate(): DateTime
+    {
+        $month = rand(1, 12);
+        $year  = rand(1900, date('Y'));
+        $day   = cal_days_in_month(CAL_GREGORIAN, $month, $year);
+        $date  = new DateTime();
+        $date->setDate($year, $month, $day);
+
+        return $date;
+    }
+
+    /**
+     * Generate a last name
+     *
+     * @return string
+     */
+    public static function generateLastName(): string
+    {
+        $lastNames = SeedValues::getLastnames();
+        $index     = rand(0, count($lastNames) - 1);
+
+        return $lastNames[$index];
+    }
+
+    /**
+     * Generate a name.
+     *
+     * @return string
+     */
+    public static function generateName(): string
+    {
+        $names = SeedValues::getNames();
+        $index = rand(0, count($names) - 1);
+
+        return $names[$index];
+    }
+
+    /**
+     * Generates an email server name.
+     *
+     * @return string
+     */
+    public static function generateEmailServerName(): string
+    {
+        $names = SeedValues::getEmailServerNames();
+        $index = rand(0, count($names) - 1);
+
+        return $names[$index];
+    }
+
+    /**
+     * Generate a domain
+     *
+     * @return string
+     */
+    public static function generateDomain(): string
+    {
+        $domain = SeedValues::getDomains();
+        $index  = rand(0, count($domain) - 1);
+
+        return $domain[$index];
+    }
+
+    /**
+     * Generate a password set.
+     *
+     * @param int $size Size of the password
+     *
+     * @return string
+     */
+    public static function generatePassword(int $size = 10): string
+    {
+        $az           = range('a', 'z');
+        $AZ           = range('A', 'Z');
+        $numbers      = range(0, 9);
+        $specialChars = SeedValues::getSpecialChars();
+        $source       = array_merge($az, $AZ, $numbers, $specialChars);
+
+        $password = '';
+        for ($i = 0; $i < $size; $i++) {
+            $password .= $source[rand(0, count($source) - 1)];
+        }
+
+        return $password;
+    }
+
+    /**
+     * Generate a set of alphanumerics chars.
+     *
+     * @param int $size
+     *
+     * @return string
+     */
+    public static function generateKey(int $size = 32): string
+    {
+        $az      = range('a', 'z');
+        $AZ      = range('A', 'Z');
+        $numbers = range(0, 9);
+
+        $source = array_merge($az, $AZ, $numbers);
+        $key    = '';
+        for ($i = 0; $i < $size; $i++) {
+            $key .= (string)$source[rand(0, count($source) - 1)];
+        }
+
+        return $key;
+    }
+
+    /**
+     * Generate a unique id.
+     *
+     * @param string $prefix
+     *
+     * @return string
+     */
+    public static function generateUniqueId(string $prefix = ''): string
+    {
+        return md5(uniqid($prefix));
+    }
+
+    /**
+     * Generate an alpha string.
+     *
+     * @param int $length
+     *
+     * @return string
+     */
+    public static function generateAlphaString(int $length = 10): string
+    {
+        $az = range('a', 'z');
+
+        $string = '';
+        for ($i = 0; $i < $length; $i++) {
+            $string .= $az[rand(0, count($az) - 1)];
+        }
+
+        return $string;
+    }
+}
---
+++ src/DataManagement/DataGenerator/SeedValues.php
@@ -0,0 +1,309 @@
+<?php
+/**
+ * @author Yenier Jimenez <yjmorales86@gmail.com>
+ */
+
+namespace Common\DataManagement\DataGenerator;
+
+/**
+ * Sets of randoms seed values used to generate random values. It's used by RandomGenerator.class
+ */
+class SeedValues
+{
+    /**
+     * Holds the `administrator` role code.
+     */
+    const ROLE_ADMIN_CODE = 'ROLE_ADMIN';
+
+    /**
+     * Holds the `administrator` role name.
+     */
+    const ROLE_ADMIN_NAME = 'ADMINISTRATOR';
+
+    /**
+     * Holds the `super administrator` role code.
+     */
+    const ROLE_SUPER_ADMIN_CODE = 'ROLE_SUPER_ADMIN';
+
+    /**
+     * Holds the `super administrator` role name.
+     */
+    const ROLE_SUPER_ADMIN_NAME = 'SUPER ADMINISTRATOR';
+
+    /**
+     * Holds the `user` role code.
+     */
+    const ROLE_USER_CODE = 'ROLE_USER';
+
+    /**
+     * Holds the `user` role name.
+     */
+    const ROLE_USER_NAME = 'USER';
+
+    /**
+     * Returns data for Roles
+     *
+     * @return array
+     */
+    public static function getRoles(): array
+    {
+        return [
+            ['code' => self::ROLE_SUPER_ADMIN_CODE, 'name' => self::ROLE_SUPER_ADMIN_NAME,],
+            ['code' => self::ROLE_ADMIN_CODE, 'name' => self::ROLE_ADMIN_NAME,],
+            ['code' => self::ROLE_USER_CODE, 'name' => self::ROLE_USER_NAME,],
+            ['code' => 'ROLE_SPECIALIST', 'name' => 'SPECIALIST',],
+            ['code' => 'ROLE_VIEWER', 'name' => 'VIEWER',],
+            ['code' => 'ROLE_USER', 'name' => 'USER',],
+            ['code' => 'ROLE_GUEST', 'name' => 'GUEST',]
+        ];
+    }
+
+    /**
+     * Returns a set of names
+     *
+     * @return string[]
+     */
+    public static function getNames(): array
+    {
+        return [
+            'Bethany',
+            'Amelia',
+            'Olivia',
+            'Olivia',
+            'Emily',
+            'Poppy',
+            'Ava',
+            'Isabella',
+            'Jessica',
+            'Lily',
+            'Sophie',
+            ' Margaret',
+            'Samantha',
+            'Bethany',
+            'Elizabeth',
+            'Joanne',
+            'Oliver',
+            'Jake',
+            'Jack',
+            'Harry',
+            'Jacob',
+            'Charlie',
+            'George',
+            'William',
+        ];
+    }
+
+    /**
+     * Returns a set of last names
+     *
+     * @return string[]
+     */
+    public static function getLastnames(): array
+    {
+        return [
+            'Smith',
+            'Johnson',
+            'Williams',
+            'Jones',
+            'Davis',
+            'Brown',
+            'Miller',
+            'Cabrera',
+            'Perez',
+            'Jimenez',
+            'Lorenzo',
+            'Gonzalez',
+            'Wilson',
+            'Cabrera',
+            'Garcia',
+            'Arias',
+        ];
+    }
+
+    /**
+     * Returns a set of phones codes
+     *
+     * @return string[]
+     */
+    public static function getPhonesCodes(): array
+    {
+        return ['305', '303', '786', '720', '214', '813',];
+    }
+
+    /**
+     * Returns a set of domains.
+     *
+     * @return string[]
+     */
+    public static function getEmailDomains(): array
+    {
+        return ['gmail.com', 'hotmail.com', 'aol.com', 'yahoo.es', 'enterprise.uk', 'nauta.cu',];
+    }
+
+    /**
+     * Returns set of prefix of street types.
+     *
+     * @return string[]
+     */
+    public static function getTypeSt(): array
+    {
+        return [
+            'St.',
+            'Street',
+            'Ct.',
+            'Rd',
+            'Dr.',
+            'Ave',
+            'Pl',
+        ];
+    }
+
+    /**
+     * Returns a set of Street names.
+     *
+     * @return string[]
+     */
+    public static function getStNames(): array
+    {
+        return [
+            'Killian',
+            'North Miami',
+            'Calais',
+            'Lincoln',
+            'Normandy',
+            'Humbolt',
+            'North Denver',
+
+        ];
+    }
+
+    /**
+     * Returns a set of cities names.
+     *
+     * @return string[]
+     */
+    public static function getCities(): array
+    {
+        return [
+            'Miami',
+            'Denver',
+            'Colorado Springs',
+            'Hialeah',
+            'Kendal',
+            'Cape Star',
+            'New York',
+            'San Diego',
+            'St. Louis',
+            'St. Lake City',
+        ];
+    }
+
+    /**
+     * Returns a set of states names.
+     *
+     * @return string[]
+     */
+    public static function getStates(): array
+    {
+        return [
+            'FL',
+            'NY',
+            'CO',
+            'OK',
+            'CA',
+            'TX',
+            'AL',
+            'WY',
+            'IL',
+            'NC',
+        ];
+    }
+
+
+    /**
+     * Returns a set of domains.
+     *
+     * @return string[]
+     */
+    public static function getDomains(): array
+    {
+        return [
+            'com',
+            'cu',
+            'us',
+            'org',
+            'fr',
+            'it',
+        ];
+    }
+
+    /**
+     * Returns a set of special chars
+     *
+     * @return string[]
+     */
+    public static function getSpecialChars(): array
+    {
+        return [
+            ' ',
+            '-!',
+            '*',
+            '/',
+            '+',
+            '.',
+            '(',
+            ')',
+            ';',
+            ',',
+            ':',
+            '<',
+            '>',
+            '|',
+            '@',
+            '[',
+            ']',
+            '#',
+            '$',
+            '%',
+            '&',
+            '^'
+        ];
+    }
+
+    /**
+     * Returns a set of email servers.
+     *
+     * @return string[]
+     */
+    public static function getEmailServerNames(): array
+    {
+        return [
+            'gmail',
+            'hotmail',
+            'aol',
+            'yahoo',
+        ];
+    }
+
+    /**
+     * Generates a letter dictionary.
+     *
+     * @return string
+     */
+    protected static function _getLetters(): string
+    {
+        return 'qwertyuioplkjhgfdsazxcvbnm';
+    }
+
+    /**
+     * Generates a letter.
+     *
+     * @return string
+     */
+    public static function randomLetter(): string
+    {
+        $dictionary = self::_getLetters();
+
+        return $dictionary[rand(0, strlen($dictionary) - 1)];
+    }
+}
