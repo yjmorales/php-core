7c379230ff67ff9c1f4fcdd09d40f0933ba792cb
yjimenez <yjimenez@sonnysdirect.com>
2022-12-28 14:27:06 -0500

    Recaptcha v3: Validate by sent token.

--- src/Security/AntiSpam/ReCaptcha/v3/ReCaptchaV3Validator.php
+++ src/Security/AntiSpam/ReCaptcha/v3/ReCaptchaV3Validator.php
@@ -5,6 +5,7 @@
 
 namespace Common\Security\AntiSpam\ReCaptcha\v3;
 
+use Common\DataManagement\Validator\DataValidator;
 use Common\Security\AntiSpam\ReCaptcha\v3\ClientToken\IClientTokenRetriever;
 use Common\Security\AntiSpam\ReCaptcha\v3\Exception\ReCaptchaV3Exception;
 use Illuminate\Support\Arr;
@@ -58,6 +59,24 @@ class ReCaptchaV3Validator
         $this->_tokenExtractor          = $tokenExtractor;
     }
 
+
+    /**
+     * Validates if the request is performed by as real user or not.
+     *
+     * @param string $userAction Holds the action name performed by client and supposed to be validated.     *
+     *
+     * @return bool True if is valid, means the action is performed by real user. Otherwise false, means it is a robot.
+     *
+     * @throws ReCaptchaV3Exception
+     * @link       https://developers.google.com/recaptcha/docs/verify#api-response
+     *
+     * @deprecated Use validateByUserAction instead.
+     */
+    public function validate(string $userAction): bool
+    {
+        return $this->validateByUserAction($userAction);
+    }
+
     /**
      * Validates if the request is performed by as real user or not.
      *
@@ -69,7 +88,7 @@ class ReCaptchaV3Validator
      *
      * @throws ReCaptchaV3Exception
      */
-    public function validate(string $userAction): bool
+    public function validateByUserAction(string $userAction): bool
     {
         try {
             $clientToken = $this->_tokenExtractor->getToken();
@@ -86,7 +105,40 @@ class ReCaptchaV3Validator
             $isValid &= null !== $action;
             $isValid &= $action === $userAction;
 
-            return $isValid;
+            return (bool)$isValid;
+        } catch (Exception $e) {
+            throw new ReCaptchaV3Exception('Unable to verify if the re-captcha v3 token is valid or not.', 0, $e);
+        }
+    }
+
+    /**
+     * Validates that a token is valid or not. The token must be submitted via POST
+     *
+     * @return bool
+     * @throws ReCaptchaV3Exception
+     */
+    public function validateToken(): bool
+    {
+        $url   = $this->_googleUrlTokenValidator;
+        $token = $_POST['g-recaptcha-response'];
+        $valid = (new DataValidator())->isValidString($token);
+        if (!$valid) {
+            throw new ReCaptchaV3Exception('Unable to verify if the re-captcha v3 token is valid or not. The token was not sent via POST');
+        }
+
+        try {
+            $data    = ['secret' => $this->_secretKey, 'response' => $token, 'remoteip' => $_SERVER['REMOTE_ADDR']];
+            $options = [
+                'http' => [
+                    'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+                    'method'  => 'POST',
+                    'content' => http_build_query($data)
+                ]
+            ];
+            $context = stream_context_create($options);
+            $result  = file_get_contents($url, false, $context);
+
+            return (bool)json_decode($result)->success;
         } catch (Exception $e) {
             throw new ReCaptchaV3Exception('Unable to verify if the re-captcha v3 token is valid or not.', 0, $e);
         }
